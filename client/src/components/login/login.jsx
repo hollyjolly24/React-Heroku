f4742bcc98ead41b87e0caa76d4013061bc75c42
hollyjolly24 <55306892+hollyjolly24@users.noreply.github.com>
2021-11-28 20:40:43 -0700

    Login Design Edits

--- client/src/components/login/login.jsx
+++ client/src/components/login/login.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { loginUser } from "../../actions/authActions";
 import classnames from "classnames";
+import Navbar from "../navbar/navbar";
+import Footer from "../footer/footer";
+import loginStyles from "./login.module.css";
 
 class Login extends Component {
   constructor() {
@@ -16,7 +18,7 @@ class Login extends Component {
   }
 
   componentDidMount() {
-    // If logged in and user navigates to Login page, should redirect them to dashboard
+    // If logged in and user navigates to Login page redirect them to dashboard
     if (this.props.auth.isAuthenticated) {
       this.props.history.push("/dashboard");
     }
@@ -47,74 +49,64 @@ this.props.loginUser(userData); // since we handle the redirect within our compo
 render() {
     const { errors } = this.state;
 return (
-      <div className="container">
-        <div style={{ marginTop: "4rem" }} className="row">
-          <div className="col s8 offset-s2">
-            <Link to="/" className="btn-flat waves-effect">
-              <i className="material-icons left">keyboard_backspace</i> Back to
-              home
-            </Link>
-            <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-              <h4>
-                <b>Login</b> below
-              </h4>
-              <p className="grey-text text-darken-1">
-                Don't have an account? <Link to="/register">Register</Link>
-              </p>
-            </div>
-            <form noValidate onSubmit={this.onSubmit}>
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.email}
-                  error={errors.email}
-                  id="email"
-                  type="email"
-                  className={classnames("", {
-                    invalid: errors.email || errors.emailnotfound
-                  })}
-                />
-                <label htmlFor="email">Email</label>
-                <span className="red-text">
-                  {errors.email}
-                  {errors.emailnotfound}
-                </span>
-              </div>
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.password}
-                  error={errors.password}
-                  id="password"
-                  type="password"
-                  className={classnames("", {
-                    invalid: errors.password || errors.passwordincorrect
-                  })}
-                />
-                <label htmlFor="password">Password</label>
-                <span className="red-text">
-                  {errors.password}
-                  {errors.passwordincorrect}
-                </span>
-              </div>
-              <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-                <button
-                  style={{
-                    width: "150px",
-                    borderRadius: "3px",
-                    letterSpacing: "1.5px",
-                    marginTop: "1rem"
-                  }}
-                  type="submit"
-                  className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-                >
-                  Login
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
+  <div className={loginStyles.bGround}>
+    <Navbar/>
+    <div className={loginStyles.contactForm}>
+
+<h1 className={loginStyles.contactHeader}>
+  Login!
+</h1>
+<h2 className={loginStyles.contactInfo}>
+  Please enter your login credentials.
+</h2>
+  <form noValidate onSubmit={this.onSubmit} className={loginStyles.contactFormBody}>
+      <p>
+          <input 
+            onChange={this.onChange}
+            value={this.state.email}
+            error={errors.email}
+            id="email"
+            type="email" 
+            name="email" 
+            placeholder="Email"
+            className={classnames(loginStyles.email,{invalid: errors.email ||errors.emailnotfound})}
+            />
+          <label htmlFor="email"></label>
+          <span className={loginStyles.invalidEmail}>
+            {errors.email}
+            {errors.emailnotfound}
+          </span>
+      </p>
+
+      <p>
+          <input 
+            onChange={this.onChange}
+            value={this.state.password}
+            error={errors.password}
+            id="password"
+            type="password" 
+            name="password" 
+            placeholder="Password"
+            className={classnames(loginStyles.password,{invalid: errors.password ||errors.password})}
+            />
+          <label htmlFor="password"></label>
+          <span className={loginStyles.invalidPassword}>
+            {errors.password}
+            {errors.passwordnotfound}
+          </span>
+      </p>
+
+      <button 
+        name="submit" 
+        type="submit"
+        className={loginStyles.submitBtn}>
+          Submit
+      </button>
+      
+  </form>
+  </div>
+  <Footer/>
+  </div> 
     );
   }
 }
