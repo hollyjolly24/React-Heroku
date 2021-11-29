e9dd464ddfe1ef0d86cc72979a4ba8e1b7ff9a84
hollyjolly24 <55306892+hollyjolly24@users.noreply.github.com>
2021-11-28 18:25:20 -0700

    Finished login / Signup
    **Need to redesign login and signup

--- client/src/App.js
+++ client/src/App.js
@@ -8,21 +8,60 @@ import terms from './components/terms/terms';
 import contactUs from './components/contactUs/contactUs';
 import Footer from './components/footer/footer';
 import Navbar from './components/navbar/navbar';
+import signup from './components/signup/signup';
+import login from './components/login/login';
+import { Provider } from "react-redux";
+import store from "./store";
+import PrivateRoute from "./components/private-route/PrivateRoute";
+import Dashboard from "./components/dashboard/Dashboard";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
+
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+// Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Redirect to login
+    window.location.href = "./login";
+  }
+}
+
+
+
+
+
+
 
 
 function App() {
   return (
-  <Router>
-        <Switch>
-          <Route path='/Navbar' exact component={Navbar}/>
-          <Route path='/Footer' exact component={Footer}/>
-          <Route path='/publicHome' exact component={publicHome}/>
-          <Route path='/aboutUs'  component={aboutUs}/>
-          <Route path='/contactUs' component={contactUs}/>
-          <Route path='/terms' component={terms}/>
-          <Route path='/privacyPolicy' component={privacyPolicy}/>
-	      </Switch>
-      </Router>
+    <Provider store={store}>
+      <Router>
+            <Switch>
+            <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <Route path='/login' component={login}/>
+              <Route path='/Navbar' component={Navbar}/>
+              <Route path='/Footer' component={Footer}/>
+              <Route path='/publicHome' component={publicHome}/>
+              <Route path='/aboutUs' component={aboutUs}/>
+              <Route path='/contactUs' component={contactUs}/>
+              <Route path='/terms' component={terms}/>
+              <Route path='/privacyPolicy' component={privacyPolicy}/>
+              <Route path='/signup' component={signup}/>
+            </Switch>
+          </Router>
+      </Provider>
   );
 }
 
