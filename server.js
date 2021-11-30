e4158aa6a418ecae0403ed2b8818485c098382da
hollyjolly24 <55306892+hollyjolly24@users.noreply.github.com>
2021-11-30 11:17:55 -0700

    make it better

--- server.js
+++ server.js
@@ -29,13 +29,16 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 
 //Static assets need served if in Heroku production.
-if (process.env.NODE_ENV === "production"){
-  app.use(express.static("build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+if(process.env.NODE_ENV === "production"){
+  //If the node environment is in production then set static 
+  app.use(express.static('client/build'));
+  //^^express serves that static files that are found in 'client/build'
+  const path = require('path');
+  app.get('*', (req,res) => {
+    // get * = get anything and then load index.html
+    res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
   });
+
 }
-
-
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port 
 app.listen(port, () => console.log(`Server up &&& running on port ${port} !`));
